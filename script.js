bbb0e67a88dd92290d540ba253d5b74a44cbe85d
supkadam <supriyakadam2495@gmail.com>
2025-03-07 15:35:17 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function changeTextColor(color){
+    var text = document.getElementById('head');
+    text.style.color = color;
+}
+
+function changeBgColor(bgcolor){
+    var item = document.getElementById('head');
+    item.style.backgroundColor = bgcolor;
+}
+
+function changeFont(font){
+    var item = document.getElementById('head');
+    item.style.fontFamily = font;
+}
